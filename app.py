81f6724233f6c9eb8aced1cfdaffdded59ea81ad
vuthanhym123-cloud <vuthanhym123@gmail.com>
2025-12-19 16:23:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -191,7 +191,7 @@ st.markdown("""
 st.markdown("""
 <div class="main-header">
     <h1>📘 TRỢ LÝ SOẠN GIÁO ÁN TỰ ĐỘNG (NLS)</h1>
-    <p>Tác giả: Sùng Văn Quý - Trường PTDTBT Tiểu học Đường Thượng - ĐT: 097 4109 364</p>
+    <p>Tác giả: Vù Văn Thành - Trường PTDTBT Tiểu học Đường Thượng - ĐT: 0333119488</p>
 </div>
 """, unsafe_allow_html=True)
 
@@ -347,4 +347,5 @@ if st.button("🚀 SOẠN GIÁO ÁN NGAY"):
 
 # --- CHÂN TRANG ---
 st.markdown("---")
-st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Sùng Văn Quý - Trường PTDTBT Tiểu học Đường Thượng - ĐT: 097 4109 364</div>", unsafe_allow_html=True)
+st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Vù Văn Thành - Trường PTDTBT Tiểu học Đường Thượng - ĐT: 0333119488</p>
+</div>, unsafe_allow_html=True)
